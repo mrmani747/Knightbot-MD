a3aa8574dc0e8ef23936fdd389763372e087cc9e
mrmani747 <dreambo999@gmail.com>
2025-02-04 16:39:03 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'ɪᴛᴢ_ʟᴏʏᴀʟ亗مـعصـͩـͥـͪـᷤـͬـͧــᷟـــوم亗', // Your name
+  ownerNumber: '+923033945449', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
